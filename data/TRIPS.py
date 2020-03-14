02e8c12ab51ecd056d65f0cb3fbea57a6c966234
Daniel <danielh839@gmail.com>
2020-03-13 21:57:52 -0400

    Added Service and ServiceExceptions

--- data/TRIPS.py
+++ data/TRIPS.py
@@ -8,33 +8,36 @@ import pandas as pd
 
 trips = pd.read_csv('trips.txt', index_col='trip_id')
 stop_times = pd.read_csv('stop_times.txt', index_col=None)
-stop_times['stop_time_id'] = stop_times['trip_id'] + stop_times['stop_id']
-stop_times.index = stop_times['stop_time_id']
-
-pd.DataFrame.to_csv(stop_times, 'stop_times_with_id.csv')
-
-print(stop_times.head())
-TRIPS = {'data': []}
+stop_times['id'] = stop_times['trip_id'] + stop_times['stop_id']
+stop_times.drop_duplicates('id', inplace=True)
 
 
-def process_trips(path):
+
+def process_trips():
     """ process trips """
+    trips_dict = dict()
     for i, trip in enumerate(trips.index):
         print(i)
-        stop_times_trip = list(stop_times[stop_times['trip_id'] == trip]['stop_time_id'])
-
-        TRIPS['data'].append({
+        trips_dict[trip] = {
             '_id': trip,
             'service': trips.at[trip, 'service_id'],
             'route': trips.at[trip, 'route_id'],
             'headsign': trips.at[trip, 'trip_headsign'],
             'direction': int(trips.at[trip, 'direction_id']),
-            'stopTimes': stop_times_trip
-        })
+            'stopTimes': []
+        }
 
-    print(TRIPS['data'][0])
-    with open(f'{path}.json', 'w') as fp:
-        json.dump(TRIPS, fp)
+    trip_ids = list(stop_times['trip_id'])
+    stop_time_ids = list(stop_times['id'])
+
+    for i, (trip_id, stop_time_id) in enumerate(zip(trip_ids, stop_time_ids)):
+        print(i)
+        trips_dict[trip_id]['stopTimes'].append(stop_time_id)
+
+    data = {'data': [trips_dict[trip] for trip in trips_dict.keys()]}
+    with open(f'TRIPS.json', 'w') as fp:
+        json.dump(data, fp)
+
 
 if __name__ == '__main__':
-    process_trips('TRIPS')
+    process_trips()
