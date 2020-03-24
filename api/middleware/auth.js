f22d17ba76ab8b8512e29e8b42a7931c3f671207
Daniel <danielh839@gmail.com>
2020-03-24 01:11:47 -0400

    Added Email to Context

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -6,15 +6,18 @@ const auth = (context) => {
         const token = context.req.headers.token;
         const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
         const user = decodedToken.user;
+        const email = decodedToken.email;
 
         return {
             user: user,
+            email: email,
             authenticated: true
         }
 
     } catch {
         return {
             user: null,
+            email: null,
             authenticated: false
         }
     }
