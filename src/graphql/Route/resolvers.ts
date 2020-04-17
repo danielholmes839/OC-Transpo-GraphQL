b57ffd7a266b2c5511e4034cf62f6b26bff00e37
Daniel <danielh839@gmail.com>
2020-04-17 17:32:10 -0400

    Added route type resolver

--- src/graphql/Route/resolvers.ts
+++ src/graphql/Route/resolvers.ts
@@ -1,6 +1,19 @@
 import { Route, Trip, Stop } from '../types';
 import { tripLoader, stopLoader } from '../loaders';
 
+const routeTypeString = {
+    0: "Street Car",
+    1: "Subway",
+    2: "Rail",
+    3: "Bus",
+    4: "Ferry",
+    5: "Cable Tram",
+    6: "Aerial Lift",
+    7: "Funicular",
+    11: "Trolleybus",
+    12: "Monorail"
+};
+
 export default {
     trips: (parent: Route): Promise<(Trip | Error)[]> => {
         return tripLoader.loadMany(parent.trips);
@@ -8,4 +21,8 @@ export default {
     stops: (parent: Route): Promise<(Stop | Error)[]> => {
         return stopLoader.loadMany(parent.stops);
     },
+
+    type: (parent: Route): string => {
+        return routeTypeString[parent.routeType];
+    }
 };
