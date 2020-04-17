235aeb6c3346d000ba09f985a53b96889c22941d
Daniel <danielh839@gmail.com>
2020-04-17 17:19:23 -0400

    added config parameter

--- src/graphql/loaders.ts
+++ src/graphql/loaders.ts
@@ -2,17 +2,17 @@ import * as DataLoader from 'dataloader';
 import { Model, Document } from 'mongoose';
 
 import {
-    User, FavouriteStop, Route, Stop, StopRoute,
-    StopTime, Trip, Service, ServiceException
+    User, FavouriteStop, Route, Stop, StopRoute, StopTime, Trip,
+    Service, ServiceException
 } from './types';
 
 import {
-    UserCollection, FavouriteStopCollection, RouteCollection, StopCollection, StopRouteCollection,
-    StopTimeCollection, TripCollection, ServiceCollection, ServiceExceptionCollection
+    UserCollection, FavouriteStopCollection, RouteCollection, StopCollection, StopRouteCollection, StopTimeCollection,
+    TripCollection, ServiceCollection, ServiceExceptionCollection
 } from './collections';
 
 
-const populate = async <T extends Document>(ids: string[], collection: Model<T>): Promise<T[]> => {
+const populate = async <T extends Document>(ids: string[], collection: Model<T>): Promise<(T | Error)[]> => {
     /* 
     Get multiple documents of type T from a collection of type T 
     Returned list is in the same order as ids and missing/non existent
@@ -20,26 +20,28 @@ const populate = async <T extends Document>(ids: string[], collection: Model<T>)
     console.log(`Populating ${ids.length} ${collection.modelName}`);
     let query: any = { _id: { $in: ids } };
     let documents: T[] = await collection.find(query);
-    let sorted_documents: T[] = [];
+    let sorted_documents: (T | Error)[] = [];
 
     // "re-sorting" by ids
-    let o: { [U: string]: T } = {}
+    let o = {};
     for (let document of documents) { o[document._id] = document }
-    for (let id of ids) { sorted_documents.push(o[id]) }
-
+    for (let id of ids) {
+        if (o[id]) sorted_documents.push(o[id]);
+        else sorted_documents.push(new Error(`Could not find ${collection.modelName}:${id}`));
+    }
     return sorted_documents;
 };
 
-const createDataLoader = <T extends Document>(collection: Model<T>): DataLoader<string, T> => {
+const createDataLoader = <T extends Document>(collection: Model<T>, config: DataLoader.Options<string, T> = { cache: true }): DataLoader<string, T> => {
     /* Function that creates dataloaders for MongoDB collections */
     return new DataLoader(async (ids: string[]): Promise<(T | Error)[]> => {
         return populate<T>(ids, collection);
-    }, {cache: false})
+    }, config)
 }
 
 /* Create a DataLoader for every collection in my Mongodb */
-const userLoader: DataLoader<string, User> = createDataLoader<User>(UserCollection);
-const favouriteStopLoader: DataLoader<string, FavouriteStop> = createDataLoader<FavouriteStop>(FavouriteStopCollection);
+const userLoader: DataLoader<string, User> = createDataLoader<User>(UserCollection, { cache: false });
+const favouriteStopLoader: DataLoader<string, FavouriteStop> = createDataLoader<FavouriteStop>(FavouriteStopCollection, { cache: false });
 const routeLoader: DataLoader<string, Route> = createDataLoader<Route>(RouteCollection);
 const stopLoader: DataLoader<string, Stop> = createDataLoader<Stop>(StopCollection);
 const stopRouteLoader: DataLoader<string, StopRoute> = createDataLoader<StopRoute>(StopRouteCollection);
