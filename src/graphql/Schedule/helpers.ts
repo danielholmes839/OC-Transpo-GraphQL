a676a62c70cc8b73be92a494923f8a624481f67b
Daniel <danielh839@gmail.com>
2020-04-28 13:58:18 -0400

    Added binary search to find next StopTime

--- src/graphql/Schedule/helpers.ts
+++ src/graphql/Schedule/helpers.ts
@@ -12,10 +12,18 @@ const days = {
     6: 'saturday'
 }
 
-const nextStopTime = async (stopTimeIDs: string[]): Promise<StopTime> =>  {
-    let stopTimes = <StopTime[]> await stopTimeLoader.loadMany(stopTimeIDs);
-    let trips = <Trip[]> await tripLoader.loadMany(stopTimes.map(stopTime => stopTime.trip));
-    let services = <Service[]> await serviceLoader.loadMany(trips.map(trip => trip.service));
+const currentTime = (): number => {
+    return
+}
+
+const currentDay = (): string => {
+    return
+}
+
+const nextStopTime = async (stopTimeIDs: string[]): Promise<StopTime> => {
+    let stopTimes = <StopTime[]>await stopTimeLoader.loadMany(stopTimeIDs);
+    let trips = <Trip[]>await tripLoader.loadMany(stopTimes.map(stopTime => stopTime.trip));
+    let services = <Service[]>await serviceLoader.loadMany(trips.map(trip => trip.service));
 
     let date = new Date();
     let day = days[date.getDay()];
@@ -24,7 +32,20 @@ const nextStopTime = async (stopTimeIDs: string[]): Promise<StopTime> =>  {
     let stopTime: StopTime;
     let service: Service
 
-    for (let i=0; i<stopTimes.length; i++) {
+    // Binary Search for start position
+    // There can be hundreds of stop times so its worth it to do a binary search
+    let start = 0;
+    let end = stopTimes.length - 1;
+    let middle = Math.floor((start + end) / 2);
+
+    while (start != end - 1) {
+        if (time > stopTimes[middle].time.int) start = middle;
+        else end = middle;
+        middle = Math.floor((start + end) / 2);
+    }
+
+    // Then find the next valid StopTime with service on the day
+    for (let i = end; i < stopTimes.length; i++) {
         stopTime = stopTimes[i];
         if (time < stopTime.time.int) {
             service = services[i]
