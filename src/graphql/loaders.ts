c0ddcf75d6a8575900c6b369a4ebe5e786590e91
Daniel <danielh839@gmail.com>
2020-06-19 19:39:03 -0400

    Renamed 'Collection' to 'Model

--- src/graphql/loaders.ts
+++ src/graphql/loaders.ts
@@ -6,18 +6,19 @@ import {
 } from './types';
 
 import {
-    UserCollection, FavouriteStopCollection, RouteCollection, StopCollection, StopRouteCollection, StopTimeCollection, TripCollection, ServiceCollection, ServiceExceptionCollection
-} from './collections';
+    UserModel, FavouriteStopModel, RouteModel, StopModel, 
+    StopRouteModel, StopTimeModel, TripModel, ServiceModel, ServiceExceptionModel
+} from './models';
 
 
-const populate = async <T extends Document>(ids: string[], collection: Model<T>): Promise<(T | Error)[]> => {
+const populate = async <T extends Document>(ids: string[], model: Model<T>): Promise<(T | Error)[]> => {
     /* 
-    Get multiple documents of type T from a collection of type T 
+    Get multiple documents of type T from a model of type T 
     Returned list is in the same order as ids and missing/non existent
     */
-    console.log(`Populating ${ids.length} ${collection.modelName}`);
+    console.log(`Populating ${ids.length} ${model.modelName}`);
     let query: any = { _id: { $in: ids } };
-    let documents: T[] = await collection.find(query);
+    let documents: T[] = await model.find(query);
     let sortedDocuments: (T | Error)[] = [];
 
     // "re-sorting" by ids
@@ -28,28 +29,28 @@ const populate = async <T extends Document>(ids: string[], collection: Model<T>)
 
     for (let id of ids) {
         if (o[id]) sortedDocuments.push(o[id]);
-        else sortedDocuments.push(new Error(`Could not find ${collection.modelName}:${id}`));
+        else sortedDocuments.push(new Error(`Could not find ${model.modelName}:${id}`));
     }
     return sortedDocuments;
 };
 
-const createDataLoader = <T extends Document>(collection: Model<T>, config: DataLoader.Options<string, T> = { cache: true }): DataLoader<string, T> => {
+const createDataLoader = <T extends Document>(model: Model<T>, config: DataLoader.Options<string, T> = { cache: true }): DataLoader<string, T> => {
     /* Function that creates dataloaders for MongoDB collections */
     return new DataLoader(async (ids: string[]): Promise<(T | Error)[]> => {
-        return populate<T>(ids, collection);
+        return populate<T>(ids, model);
     }, config)
 }
 
 /* Create a DataLoader for every collection in Mongodb */
-const userLoader = createDataLoader<User>(UserCollection, { cache: false });
-const favouriteStopLoader = createDataLoader<FavouriteStop>(FavouriteStopCollection, { cache: false });
-const routeLoader = createDataLoader<Route>(RouteCollection);
-const stopLoader = createDataLoader<Stop>(StopCollection);
-const stopRouteLoader = createDataLoader<StopRoute>(StopRouteCollection);
-const stopTimeLoader = createDataLoader<StopTime>(StopTimeCollection);
-const tripLoader = createDataLoader<Trip>(TripCollection);
-const serviceLoader = createDataLoader<Service>(ServiceCollection);
-const serviceExceptionLoader = createDataLoader<ServiceException>(ServiceExceptionCollection);
+const userLoader = createDataLoader<User>(UserModel, { cache: false });
+const favouriteStopLoader = createDataLoader<FavouriteStop>(FavouriteStopModel, { cache: false });
+const routeLoader = createDataLoader<Route>(RouteModel);
+const stopLoader = createDataLoader<Stop>(StopModel);
+const stopRouteLoader = createDataLoader<StopRoute>(StopRouteModel);
+const stopTimeLoader = createDataLoader<StopTime>(StopTimeModel);
+const tripLoader = createDataLoader<Trip>(TripModel);
+const serviceLoader = createDataLoader<Service>(ServiceModel);
+const serviceExceptionLoader = createDataLoader<ServiceException>(ServiceExceptionModel);
 
 export {
     userLoader, favouriteStopLoader, routeLoader, stopLoader, stopRouteLoader,
