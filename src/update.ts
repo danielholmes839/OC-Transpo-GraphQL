89f7440cb3949a4f46f52684b5caebb6832c961a
Daniel <danielh839@gmail.com>
2020-08-03 18:30:18 -0400

    Better update to database script

--- src/update.ts
+++ src/update.ts
@@ -17,13 +17,26 @@ const connectDB = async () => {
     await connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 }
 
+const delete_collection = async <T extends Document>(model: Model<T>) => {
+    try {
+        await model.collection.drop();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 const insert = async <T extends Document>(data: T[], model: Model<T>) => {
-    console.log(model.name, data.length)
+    console.log(model.collection.name, data.length)
     let chunk_size = 5000;
     for (let i = 0; i < data.length; i += chunk_size) {
         console.log(i);
         let slice: T[] = data.slice(i, i + chunk_size)
-        await model.insertMany(slice);
+        try {
+            await model.insertMany(slice);
+        } catch (error) {
+            console.log(error);
+        }
+
     }
 }
 
@@ -35,31 +48,31 @@ const main = async () => {
     await connectDB();
 
     let routes: Route[] = read('ROUTES.json');
-    RouteModel.collection.drop();
+    delete_collection(RouteModel);
     await insert(routes, RouteModel);
 
     let services = read<Service>('SERVICES.json');
-    ServiceModel.collection.drop();
+    delete_collection(ServiceModel);
     await insert(services, ServiceModel);
 
     let service_exceptions = read<ServiceException>('SERVICE_EXCEPTIONS.json');
-    ServiceExceptionModel.collection.drop();
+    delete_collection(ServiceExceptionModel);
     await insert(service_exceptions, ServiceExceptionModel);
 
     let stop_routes = read<StopRoute>('STOP_ROUTES.json');
-    StopRouteModel.collection.drop();
+    delete_collection(StopRouteModel);
     await insert(stop_routes, StopRouteModel);
 
     let stops = read<Stop>('STOPS.json');
-    StopModel.collection.drop();
+    delete_collection(StopModel);
     await insert(stops, StopModel);
 
     let trips = read<Trip>('TRIPS.json');
-    TripModel.collection.drop();
+    delete_collection(TripModel);
     await insert(trips, TripModel);
 
     let stop_times = read<StopTime>('STOP_TIMES.json');
-    StopTimeModel.collection.drop();
+    delete_collection(StopTimeModel)
     await insert(stop_times, StopTimeModel);
 
     console.log('Finished uploading');
