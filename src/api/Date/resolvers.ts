8cc82c66156acb7785fa8eea9a2ec85cde48d7f8
Daniel <danielh839@gmail.com>
2020-09-10 14:21:12 -0400

    Use string literal not 'String'

--- src/api/Date/resolvers.ts
+++ src/api/Date/resolvers.ts
@@ -1,9 +1,9 @@
 import { Date } from './types';
 
 export default {
-    string: ({ year, month, day }: Date): String => {
-        let mm: String | number = (month < 10) ? `0${month}` : month;
-        let dd: String | number = (day < 10) ? `0${day}` : day;
+    string: ({ year, month, day }: Date): string => {
+        let mm: string | number = (month < 10) ? `0${month}` : month;
+        let dd: string | number = (day < 10) ? `0${day}` : day;
         return `${year}/${mm}/${dd}`;
     }
 }
