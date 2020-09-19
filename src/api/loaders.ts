f56af8f340e689220d8171d575e8d602af58aa5e
Daniel <danielh839@gmail.com>
2020-09-19 00:15:15 -0400

    cleaner code

--- src/api/loaders.ts
+++ src/api/loaders.ts
@@ -1,81 +1,43 @@
 import DataLoader from 'dataloader';
 import { Model, Document } from 'mongoose';
+import { User, FavouriteStop, Route, Stop, StopRoute, StopTime, Trip, Service, ServiceException } from './types';
+import { UserModel, FavouriteStopModel, RouteModel, StopModel, StopRouteModel, StopTimeModel, TripModel, ServiceModel, ServiceExceptionModel } from './models';
 
-import {
-    User, FavouriteStop, Route, Stop, StopRoute, StopTime, Trip, Service, ServiceException
-} from './types';
+type Result<T> = T | Error;  // Makes it a bit easier to read the code
 
-import {
-    UserModel, FavouriteStopModel, RouteModel, StopModel,
-    StopRouteModel, StopTimeModel, TripModel, ServiceModel, ServiceExceptionModel
-} from './models';
-
-
-const authorize_document = <T extends Document>(document: T | Error, user: string, get_user: (document: T) => string) => {
-    /* Authenticates documents to belong to the user  */
-    // The document is null
-    if (document instanceof Error) {
-        return document;
-    }
-
-    // The document does not belong to the user
-    else if (get_user(document) != user) {
-        return new Error('Not authorized');
-    }
-
-    // The document is valid
-    else {
-        return document;
-    }
-}
-
-const populate = async <T extends Document>(ids: string[], model: Model<T>): Promise<(T | Error)[]> => {
-    /*  DataLoader Populate for Mongoose */
+const populate = async <T extends Document>(ids: string[], model: Model<T>): Promise<Result<T>[]> => {
+    /*  Populates documents in a MongoDB collection (Needs to sort the documents) */
     console.log(`Populating ${ids.length} ${model.modelName}`);
-    let query: any = { _id: { $in: ids } };
-    let documents: T[] = await model.find(query);
-    let sorted_documents: (T | Error)[] = [];
-
-    // 're-sorting' by ids
-    let o = {};
-    for (let document of documents) {
-        o[document._id] = document;
-    }
-
-    for (let id of ids) {
-        if (o[id]) {
-            sorted_documents.push(o[id]);
-        }
-        else {
-            sorted_documents.push(new Error(`Could not find ${model.modelName}:${id}`));
-        }
-    }
-
-    return sorted_documents;
+    let temp = {};
+    (await model.find(<any>{ _id: { $in: ids } })).forEach(document => temp[document._id] = document);
+    return ids.map(id => (temp[id] == null) ? new Error(`Could not find ${model.modelName}:${id}`) : temp[id]);
 };
 
-const createDataLoader = <T extends Document>(model: Model<T>, config: DataLoader.Options<string, T> = { cache: true }): DataLoader<string, T> => {
-    /* Function that creates dataloaders for MongoDB collections */
-    return new DataLoader(async (ids: string[]): Promise<(T | Error)[]> => {
-        return populate<T>(ids, model);
-    }, config)
+const authorizeDocument = <T extends Document>(document: Result<T>, userId: string, ownerId: (document: T) => string): Result<T> => {
+    /* Compare the document user/owner ID with the authenticated user ID */
+    if (document instanceof Error) { return document; }
+    let authorized: boolean = userId == ownerId(document);
+    return authorized ? document : new Error('Authorization Required');
 }
 
-const createAuthDataLoader = <T extends Document>(user: string, get_user: (document: T) => string, model: Model<T>, config: DataLoader.Options<string, T> = { cache: false }): DataLoader<string, T> => {
-    /* Function that creates dataloaders for MongoDB collections */
-    return new DataLoader(async (ids: string[]): Promise<(T | Error)[]> => {
+const createDataLoader = <T extends Document>(model: Model<T>, config: DataLoader.Options<string, T> = { cache: true }): DataLoader<string, T> => {
+    /* Create dataloaders for MongoDB collections */
+    let batchLoad = async (ids: string[]): Promise<Result<T>[]> => populate<T>(ids, model);
+    return new DataLoader<string, T>(batchLoad, config);
+}
+
+const createAuthDataLoader = <T extends Document>(user: string, ownerId: (document: T) => string, model: Model<T>, config: DataLoader.Options<string, T> = { cache: false }): DataLoader<string, T> => {
+    /* 
+        Create dataloaders for MongoDB collections 
+        And ensure that the requested documents belong to the user 
+    */
+    return new DataLoader<string, T>(async (ids: string[]): Promise<Result<T>[]> => {
         const documents = await populate<T>(ids, model);
-        return documents.map(document => authorize_document(document, user, get_user));
+        return documents.map(document => authorizeDocument(document, user, ownerId));
     }, config);
 }
 
-
-interface AuthLoaders {
-    userLoader?: DataLoader<string, User>;
-    favouriteStopLoader?: DataLoader<string, FavouriteStop>;
-}
-
-interface OpenLoaders {
+type OpenLoaders = {
     routeLoader: DataLoader<string, Route>;
     stopLoader: DataLoader<string, Stop>;
     stopRouteLoader: DataLoader<string, StopRoute>;
@@ -85,7 +47,22 @@ interface OpenLoaders {
     serviceExceptionLoader: DataLoader<string, ServiceException>;
 }
 
-type Loaders = OpenLoaders & AuthLoaders
+type AuthLoaders = {
+    userLoader: DataLoader<string, User>;
+    favouriteStopLoader: DataLoader<string, FavouriteStop>;
+}
+
+type Loaders = {
+    routeLoader: DataLoader<string, Route>;
+    stopLoader: DataLoader<string, Stop>;
+    stopRouteLoader: DataLoader<string, StopRoute>;
+    stopTimeLoader: DataLoader<string, StopTime>;
+    tripLoader: DataLoader<string, Trip>;
+    serviceLoader: DataLoader<string, Service>;
+    serviceExceptionLoader: DataLoader<string, ServiceException>;
+    userLoader?: DataLoader<string, User>;
+    favouriteStopLoader?: DataLoader<string, FavouriteStop>;
+}
 
 const createAuthLoaders = (user: string): AuthLoaders => {
     /* Loader for documents that require authentication (belong to the authenticated user) */
@@ -116,7 +93,7 @@ const createLoaders = (authenticated: boolean, user: string): Loaders => {
             ...createOpenLoaders()
         }
     } else {
-        return createOpenLoaders()
+        return createOpenLoaders();
     }
 }
 
