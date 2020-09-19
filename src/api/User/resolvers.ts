b902178d5b3f22ff774ca2083455cd501d592fcf
Daniel <danielh839@gmail.com>
2020-09-18 23:41:57 -0400

    removed always null password field

--- src/api/User/resolvers.ts
+++ src/api/User/resolvers.ts
@@ -3,7 +3,6 @@ import { User, FavouriteStop } from 'api/types';
 
 // User Resolvers
 export default {
-    password: () => null,   // must always be null
     favouriteStops: (parent: User, _: void, context: Context): Promise<(FavouriteStop | Error)[]> => {
         const { favouriteStopLoader } = context.loaders;
         return favouriteStopLoader.loadMany(parent.favouriteStops);
