b9228ed645292fc0a71bd3ae1f1f0dd8f72c4ee5
Daniel <danielh839@gmail.com>
2020-09-19 17:09:45 -0400

    comments

--- src/api/Query/resolvers.ts
+++ src/api/Query/resolvers.ts
@@ -5,24 +5,23 @@ import { Stop, StopTime, StopRoute, Route, Trip, User, Service } from 'api/types
 import { UserModel, StopModel } from 'api/models';
 import { Login, StopSearch } from './types'
 
-// Query Resolvers
-type GetQuery = { id: string };
-type GetManyQuery = { ids: string[] };
+type Get = { id: string };
+type GetMany = { ids: string[] };
 
+// Query Resolvers
 export default {
 	// Stop Queries
-	Stop_get: (_: void, { id }: GetQuery, context: Context): Promise<Stop> => {
+	Stop_get: (_: void, { id }: Get, context: Context): Promise<Stop> => {
 		const { stopLoader } = context.loaders
 		return stopLoader.load(id);
 	},
 
-	Stop_getMany: (_: void, { ids }: GetManyQuery, context: Context): Promise<(Stop | Error)[]> => {
+	Stop_getMany: (_: void, { ids }: GetMany, context: Context): Promise<(Stop | Error)[]> => {
 		const { stopLoader } = context.loaders
 		return stopLoader.loadMany(ids);
 	},
 
-	Stop_search: async (_: void, { name, limit }: StopSearch): Promise<Stop[]> => {
-		if (limit == null) limit = 3;
+	Stop_search: async (_: void, { name, limit=3 }: StopSearch): Promise<Stop[]> => {
 		// Search by stop code first
 		let stops = await StopModel.find({ code: name });
 		if (stops.length !== 0) {
@@ -38,56 +37,56 @@ export default {
 	},
 
 	// Route Queries
-	Route_get: (_: void, { id }: GetQuery, context: Context): Promise<Route> => {
+	Route_get: (_: void, { id }: Get, context: Context): Promise<Route> => {
 		const { routeLoader } = context.loaders
 		return routeLoader.load(id);
 	},
 
-	Route_getMany: (_: void, { ids }: GetManyQuery, context: Context): Promise<(Route | Error)[]> => {
+	Route_getMany: (_: void, { ids }: GetMany, context: Context): Promise<(Route | Error)[]> => {
 		const { routeLoader } = context.loaders;
 		return routeLoader.loadMany(ids);
 	},
 
 	// StopRoute Queries
-	StopRoute_get: (_: void, { id }: GetQuery, context: Context): Promise<StopRoute> => {
+	StopRoute_get: (_: void, { id }: Get, context: Context): Promise<StopRoute> => {
 		const { stopRouteLoader } = context.loaders;
 		return stopRouteLoader.load(id);
 	},
 
-	StopRoute_getMany: (_: void, { ids }: GetManyQuery, context: Context): Promise<(StopRoute | Error)[]> => {
+	StopRoute_getMany: (_: void, { ids }: GetMany, context: Context): Promise<(StopRoute | Error)[]> => {
 		const { stopRouteLoader } = context.loaders;
 		return stopRouteLoader.loadMany(ids);
 	},
 
 	// Trip Queries
-	Trip_get: (_: void, { id }: GetQuery, context: Context): Promise<Trip> => {
+	Trip_get: (_: void, { id }: Get, context: Context): Promise<Trip> => {
 		const { tripLoader } = context.loaders;
 		return tripLoader.load(id);
 	},
 
-	Trip_getMany: (_: void, { ids }: GetManyQuery, context: Context): Promise<(Trip | Error)[]> => {
+	Trip_getMany: (_: void, { ids }: GetMany, context: Context): Promise<(Trip | Error)[]> => {
 		const { tripLoader } = context.loaders;
 		return tripLoader.loadMany(ids);
 	},
 
 	// Service Queries
-	Service_get: (_: void, { id }: GetQuery, context: Context): Promise<Service> => {
+	Service_get: (_: void, { id }: Get, context: Context): Promise<Service> => {
 		const { serviceLoader } = context.loaders;
 		return serviceLoader.load(id);
 	},
 
-	Service_getMany: (_: void, { ids }: GetManyQuery, context: Context): Promise<(Service | Error)[]> => {
+	Service_getMany: (_: void, { ids }: GetMany, context: Context): Promise<(Service | Error)[]> => {
 		const { serviceLoader } = context.loaders;
 		return serviceLoader.loadMany(ids);
 	},
 
 	// StopTime Queries
-	StopTime_get: (_: void, { id }: GetQuery, context: Context): Promise<StopTime> => {
+	StopTime_get: (_: void, { id }: Get, context: Context): Promise<StopTime> => {
 		const { stopTimeLoader } = context.loaders;
 		return stopTimeLoader.load(id);
 	},
 
-	StopTime_getMany: (_: void, { ids }: GetManyQuery, context: Context): Promise<(StopTime | Error)[]> => {
+	StopTime_getMany: (_: void, { ids }: GetMany, context: Context): Promise<(StopTime | Error)[]> => {
 		const { stopTimeLoader } = context.loaders;
 		return stopTimeLoader.loadMany(ids);
 	},
@@ -100,11 +99,11 @@ export default {
 		return userLoader.load(context.user);
 	},
 
-	User_login: async (_: void, login: Login, context: Context): Promise<String> => {
+	User_login: async (_: void, { email, password }: Login): Promise<String> => {
 		/* Login a user and return a json web token */
-		const user: User = await UserModel.findOne({ email: login.email });						// Find existing user
+		const user: User = await UserModel.findOne({ email: email });						// Find existing user
 		if (!user) throw new Error('Incorrect username or password');							// No existing user
-		const authenticated: boolean = await bcrypt.compare(login.password, user.password);		// Check if passwords match
+		const authenticated: boolean = await bcrypt.compare(password, user.password);		// Check if passwords match
 		if (!authenticated) throw new Error('Incorrect username or password');					// Password don't match
 
 		// Return the user, jwt, expiration
