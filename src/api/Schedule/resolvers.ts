6ec8da0253dc72c129e4dd250a4c99d31b10277b
Daniel <danielh839@gmail.com>
2020-09-20 11:25:54 -0400

    Fixed a bug

--- src/api/Schedule/resolvers.ts
+++ src/api/Schedule/resolvers.ts
@@ -1,5 +1,6 @@
-import { StopTime, Service } from '../types';
 import { Context } from 'middleware'
+import { StopTime, Service } from '../types';
+import { stopTimeServiceToday, stopTimeServiceTommorow, getCurrentTime } from 'helpers';
 
 type Next_args = {
     limit: number;
@@ -15,21 +16,6 @@ const days = {
     6: 'saturday'
 }
 
-const getCurrentTime = (): number => {
-    let date = new Date();
-    return (date.getHours() * 60) + date.getMinutes();
-}
-
-const serviceToday = (stopTime: StopTime, service: Service, today: string, tommorow: string): boolean => {
-    // Service today
-    return !stopTime.serviceIsNextDay && service[today]
-}
-
-const serviceTommorow = (stopTime: StopTime, service: Service, today: string, tommorow: string): boolean => {
-    // Service tommorow
-    return (!stopTime.serviceIsNextDay && service[tommorow]) || (stopTime.serviceIsNextDay && service[today]);
-}
-
 // Schedule Resolvers
 export default {
     all: async (stopTimeIds: string[], _: void, context: Context): Promise<(StopTime | Error)[]> => {
@@ -39,26 +25,30 @@ export default {
 
     next: async (stopTimeIds: string[], { limit = 1 }: Next_args, context: Context): Promise<(StopTime | Error)[]> => {
         const { stopTimeLoader, serviceLoader } = context.loaders;
-        let date: Date = new Date();
+        let day: number = new Date().getDay();
         let time: number = getCurrentTime();
-        let today: string = days[date.getDay()];
-        let tommorow: string = days[(date.getDay() + 1) % 7];
+
+        // access service with service[yesterday], service[tommorow], service[tommorow],
+        let yesterday: string = days[(day + 6) % 7]
+        let today: string = days[day];
+        let tommorow: string = days[(day + 1) % 7];
+
         let stopTimes: StopTime[] = <StopTime[]>await stopTimeLoader.loadMany(stopTimeIds);
         let services: Service[] = <Service[]>await serviceLoader.loadMany(stopTimes.map(stopTime => stopTime.service));
 
         let index = 0
         let valid: StopTime[] = stopTimes.filter((stopTime: StopTime, i: number) => {
             let service: Service = services[i];
-            if (stopTime.time >= time && serviceToday(stopTime, service, today, tommorow)) {
-                index += 1;
+            if (stopTime.time >= time && stopTimeServiceToday(stopTime, service, today, yesterday)) {
                 return true;
-            } else if (stopTime.time < time && serviceTommorow(stopTime, service, today, tommorow)) {
+            } else if (stopTime.time < time && stopTimeServiceTommorow(stopTime, service, today, tommorow)) {
+                index += 1;
                 return true;
             }
             return false;
-        })
+        });
 
-        index += 1
+        // Slice valid stop times into a list
         let next: StopTime[] = valid.slice(index, index + limit);
         if (next.length < limit) {
             return next.concat(valid.slice(0, limit));
